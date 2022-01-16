862439e6cdfd1ea7b2fb480d16a9527f59682ab4
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-01-16 12:35:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import requests
 import urllib
 
 
-from telethon.sync import TelegramClient ,events ,Button ,
+from telethon.sync import TelegramClient ,events ,Button 
 api_id = 4091096
 api_hash = '6bb0682b4af56456201c3b9d8b99c94a'
 bot_token = '2025919134:AAGAyAXR9hTJZu6v75-5ho8ao95mcppXacU'
