8403a3c4bf7692e4103042ded44a01a460dba8de
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-01-20 16:57:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,10 +20,16 @@ images = [{'name':image['name'],'url':image['url'],'id':image['id']} for image i
 userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 \
     Safari/537.36'
-
+API_TEXT ="blaaa"
 
 @bot.on_message(filters.command(commands=['meme']) & filters.private)
-def meme1(client,message):
+def meme1(client,message,bot):
     message.reply_text(text="yooo")
+    api = await bot.ask(
+        chat.id
+    )
+    if await is_cancel(message, api.text,API_TEXT):
+        return
+    message.reply_text(api)
     
 bot.run()    
